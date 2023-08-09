4753d2ba3565451d107db5741bedfc91b864f949
Дмитрий <Polkovnikov13@gmail.com>
2023-08-09 14:01:32 +0000

    add methods

--- pages/js/script.js
+++ pages/js/script.js
@@ -1,16 +1,33 @@
 $(document).ready(function () {
-    const canvas = document.getElementById("myCanvas");
-    const context = canvas.getContext("2d");
-    const points = [];
+    const radius = 20;
+    const nodes = [];
+    const edges = [];
 
-    function drawPoints() {
+    function repaint() {
+        const canvas = document.getElementById("myCanvas");
+        const context = canvas.getContext("2d");
         context.clearRect(0, 0, canvas.width, canvas.height);
+        drawEdges(context);
+        drawNodes(context);
+    }
+
+    function drawNodes(context) {
         context.fillStyle = "black";
-        points.forEach(point => {
+        nodes.forEach(point => {
             context.beginPath();
-            context.arc(point.x, point.y, 20, 0, 2 * Math.PI);
+            context.arc(point.x, point.y, radius, 0, 2 * Math.PI);
             context.stroke();
         });
+    }
+
+    function drawEdges(context) {
+        context.fillStyle = "black";
+        edges.forEach(edge => {
+            context.beginPath();
+            context.moveTo(nodes[edge.left].x, nodes[edge.left].y)
+            context.lineTo(nodes[edge.rigth].x, nodes[edge.rigth].y)
+            context.stroke();
+        })
     }
 
     function handleCanvasClick(event) {
@@ -18,8 +35,20 @@ $(document).ready(function () {
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;
 
-        points.push({ x, y });
-        drawPoints();
+        nodes.push({ x, y });
+        repaint();
+    }
+
+    function findNodeIndex(x, y) {
+        for (let i = 0; i < nodes.length; i++) {
+            const node = nodes[i];
+            const distance = Math.sqrt((pointX - node.x) ** 2 + (pointY - node.y) ** 2);
+            
+            if (distance <= radius) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 
